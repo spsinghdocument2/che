6105b5d397f2a4e1d742fbcf850aa723ab7ecd21
spsinghdocument2 <snrinfocomspsingh@gmail.com>
2019-01-26 08:49:34 -0500

    first commit

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('hello saurabh pratap singh and node js');
 });
+app.get('/sp', function (req, res) {
+    res.send('hello sp singh and node js');
+});
 
 app.listen(3000, function () {
     console.log("server start");
