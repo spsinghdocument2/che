e525122c3b8dd74d2b5d09225d08822225e46687
spsinghdocument2 <snrinfocomspsingh@gmail.com>
2019-01-26 09:06:31 -0500

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 ﻿var express = require('express');
 var app = express();
+var port = process.env.port || 1337;
 
 app.get('/', function (req, res) {
-    res.send('hello saurabh pratap singh and node js');
+    res.send('hello arvind pratap singh and node js');
 });
 app.get('/sp', function (req, res) {
-    res.send('hello sp singh and node js');
+    res.send('hello ram singh and node js');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log("server start");
 });
